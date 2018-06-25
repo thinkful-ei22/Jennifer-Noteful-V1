48617b362108c734b721c57c9ad7a2782da47bc5
Jennifer Colna <jennifer.colna@gmail.com>
2018-06-25 14:18:39 -0500

    can get from id number, nodemon installed

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ app.get('/api/notes', (req, res) => {
   res.json(data);
 });
 
+app.get('/api/notes/:id', (req, res)=>{
+  const id = req.params.id;
+  const result = data.find(item=>item.id===Number(id));
+  res.json(result);
+});
+
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
