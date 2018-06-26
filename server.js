1506ccf8533bfe91a25420f7e3e46e80c1d9a20c
Jennifer Colna <jennifer.colna@gmail.com>
2018-06-26 13:28:00 -0500

    added scratch/queries.js

--- server.js
+++ server.js
@@ -39,9 +39,6 @@ app.get('/api/notes/:id', (req, res)=>{
   res.json(result);
 });
 
-app.get('/boom', (req, res, next) => {
-  throw new Error('Boom!!');
-});
 
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
