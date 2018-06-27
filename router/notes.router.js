615cd65ffdb946c09ec1616dd729087b5601eedf
Jennifer Colna <jennifer.colna@gmail.com>
2018-06-27 13:45:49 -0500

    added post

--- router/notes.router.js
+++ router/notes.router.js
@@ -8,6 +8,25 @@ const data = require('../db/notes');
 const simDB = require('../db/simDB');
 const notes = simDB.initialize(data);
 
+router.post('/',(req, res, next)=>{
+  const {title, content} = req.body;
+  const newItem = {title, content};
+  if (!newItem.title) {
+    const err = new Error('Missing title in request body');
+    err.status = 400;
+    return next(err);
+  }
+  notes.create(newItem, (err, item) => {
+    if(err) {
+      return next(err);
+    }
+    if (item) {
+      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+    }else{
+      next();
+    }
+  });
+});
 router.get('/', (req, res, next) => {
   const { searchTerm } = req.query;
   notes.filter(searchTerm, (err, list) => {
